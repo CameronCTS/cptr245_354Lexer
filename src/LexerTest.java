e9c76b0530686ecf18ae4bb31c793d8f7e18551e
Cameron Smith <cameron.smith@wallawalla.edu>
2017-11-03 10:12:03 -0700

    Added Oberon.java and wrote first passing test in LexerTest.java.

--- src/LexerTest.java
+++ src/LexerTest.java
@@ -1,5 +1,15 @@
+import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class LexerTest {
+
+    @Test
+    public void testGetLexeme() {
+
+        Lexer lex = new Lexer();
+        Token token = lex.GetToken ();
+        assertEquals(token.GetLexeme(), "5");
+    }
 
 }
